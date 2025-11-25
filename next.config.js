6dd062959fa8a4cd694fa65d8cfc1b1dbcdfa2a3
Sam Muriithi <105632389+samurai9699@users.noreply.github.com>
2025-11-25 23:22:19 +0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Optimize for WebContainer environments (Bolt.new, StackBlitz)
-  webpack: (config, { isServer }) => {
-    // Disable code splitting to prevent chunk loading issues
-    if (!isServer) {
-      config.optimization.splitChunks = false;
-      config.optimization.runtimeChunk = false;
-    }
-    return config;
-  },
   // Disable image optimization if causing issues
   images: {
     unoptimized: true,
