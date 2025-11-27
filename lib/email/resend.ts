f68c6d54241fecf9308d049f1136140174c45e03
Sam Muriithi <105632389+samurai9699@users.noreply.github.com>
2025-11-28 01:07:28 +0300

    Updated resend.ts

--- lib/email/resend.ts
+++ lib/email/resend.ts
@@ -1,10 +1,8 @@
 import { Resend } from 'resend';
 
-if (!process.env.RESEND_API_KEY) {
-    throw new Error('RESEND_API_KEY is not set');
-}
+const RESEND_API_KEY = process.env.RESEND_API_KEY || '';
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+export const resend = new Resend(RESEND_API_KEY);
 
 export const FROM_EMAIL = process.env.RESEND_FROM_EMAIL || 'noreply@dravikly.com';
 export const ADMIN_EMAIL = process.env.RESEND_ADMIN_EMAIL || 'admin@dravikly.com';
